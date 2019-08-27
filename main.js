2fbd6cec8335d3e93258961c2c5f51d321649ae3
gerrycadejo <gerargoza@gmail.com>
2019-08-27 18:16:49 +0200

    change

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+/*
+function intro() {
+  setTimeout(introduction, 20000);
+}
+intro();
+*/
+function introduction() {
+  document.getElementById('intro').style.display = "none";
+  document.getElementById('page1').style.display = "block";
+}
 
+introduction();
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "20vh";
+  document.getElementById("page1").style.marginLeft = "250px";
+  document.getElementById("triangle").style.display="none";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("page1").style.marginLeft = "0";
+  document.getElementById("triangle").style.display="block";
+
+}
