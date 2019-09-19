27fdfe15ad4ab0e635f5182decc1c8fdbd19e43e
gerrycadejo <gerargoza@gmail.com>
2019-09-19 15:36:12 +0200

    yey

--- main.js
+++ main.js
@@ -12,16 +12,16 @@ intro();
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "40vh";
-  document.getElementById("page1").style.marginLeft = "250px";
   document.getElementById('burger').style.display = "none";
+  document.getElementById("page1").style.marginLeft = "250px";
 
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("page1").style.marginLeft = "0";
   document.getElementById('burger').style.display = "block";
+  document.getElementById("page1").style.marginLeft = "0";
 
 }
 
