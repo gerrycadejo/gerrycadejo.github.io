58e7d332465ae081a62e891ca154e92140568998
gerrycadejo <gerargoza@gmail.com>
2019-09-26 23:37:54 +0200

    lets go

--- main2.js
+++ main2.js
@@ -1,12 +1,16 @@
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "40vh";
-  document.getElementById("page1").style.marginLeft = "250px";
-  document.getElementById('burger').style.zIndex = "0";
+  document.getElementById('burger').style.display = "none";
+  document.getElementById('scroll-indicator').style.display = "none";
+
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("page1").style.marginLeft = "0";
-  document.getElementById('burger').style.zIndex = "1";
+  document.getElementById('burger').style.display = "block";
+  document.getElementById('scroll-indicator').style.display = "block";
+
+
 }
